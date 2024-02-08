0a8aba93f6cd8e660064f3adad6c48cc0b9051f4
Aditi Rajput <aditirajput200017@gmail.com>
2024-02-08 22:44:25 +0530

    Header section i s added and can we used futher

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+import Wrapper from "../components/wrapper/Wrapper"
+import SliderHome from "../components/Slider"
+
+
+const Home = () => {
+  return (
+    <>
+      <SliderHome/>
+      <Wrapper />
+    </>
+  )
+}
+
+export default Home
