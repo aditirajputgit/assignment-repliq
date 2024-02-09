74b53332b977e14c0eefef28127567ae63b3f43f
Aditi Rajput <aditirajput200017@gmail.com>
2024-02-09 06:38:22 +0530

    Home section is ready where we have a slider which will tell the information about discount and best deals are available

---
+++ src/components/SliderCard/SlideCard.jsx
@@ -0,0 +1,26 @@
+import { Col, Container, Row } from "react-bootstrap";
+import "./slidercard.css";
+
+const SlideCard = ({title,desc,cover}) => {
+  return (
+    <>
+      <div className="home-container">
+        
+        <Container className="box">
+          <Row>
+            <Col md={6}>
+              <h1>{title}</h1>
+              <p>{desc}</p>
+              <button className="btn-primary">Visit Collections</button>
+            </Col>
+            <Col md={6}>
+              <img src={cover} alt="#" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default SlideCard
